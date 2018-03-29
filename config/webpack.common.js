3a0337adb86acc8481a737df2a6a392b240bfa43
Julián David <jdtorregrosas@gmail.com>
2018-03-29 18:55:29 -0500

    Add webpack initial configuration with angular5

---
+++ config/webpack.common.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+        {
+            test: /\.ts$/,
+            loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+        },
+        {
+            test: /\.html$/,
+            loader: 'html-loader'
+        },
+        {
+            test: /\.ts$/,
+            enforce: "pre",
+            loader: 'tslint-loader'
+        },
+        {
+          test: /\.(css|scss)$/,
+          loaders: ['to-string-loader', 'css-loader', 'sass-loader']
+        }
+    ]
+  },
+
+  plugins: [
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /\@angular(\\|\/)core(\\|\/)esm5/,
+      helpers.root('./src'), // location of your src
+      {} // a map of your routes
+    ),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
